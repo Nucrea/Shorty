204efb391624519af048ab9c20deb05ae3074fb3
Sergey Chubaryan <serghio2@yandex.ru>
2025-03-19 17:30:14 +0300

    cache images with etag

--- server/image_resolve.go
+++ server/image_resolve.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"net/http"
 	"shorty/src/services/image"
 
 	"github.com/gin-gonic/gin"
@@ -24,7 +25,18 @@ func (s *server) ImageResolve(c *gin.Context) {
 		return
 	}
 
-	img, err := s.ImageService.GetImageBytes(c, id, isThumbnail)
+	meta, err := s.ImageService.GetImageMetadata(c, id)
+	if err != nil {
+		s.pages.InternalError(c)
+		return
+	}
+
+	if oldEtag := c.GetHeader("If-None-Match"); oldEtag == meta.Hash {
+		c.AbortWithStatus(http.StatusNotModified)
+		return
+	}
+
+	imageBytes, err := s.ImageService.GetImageBytes(c, id, isThumbnail)
 	if err == image.ErrImageNotFound {
 		s.pages.NotFound(c)
 		return
@@ -34,5 +46,8 @@ func (s *server) ImageResolve(c *gin.Context) {
 		return
 	}
 
-	c.Data(200, "image/jpeg", img)
+	c.Header("ETag", meta.Hash)
+	c.Header("Cache-Control", "public, max-age=300")
+
+	c.Data(200, "image/jpeg", imageBytes)
 }
